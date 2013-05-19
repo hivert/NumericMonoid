8deaf5a747b7c302d6cd84dd55168b07a1631282
Florent Hivert <Florent.Hivert@lri.fr>
2013-05-19 11:18:46 +0200

    Rewrote C elision

--- mongen.c
+++ mongen.c
@@ -1,1 +1,10 @@
-mongen.cilk
+#define C_ELISION
+#define cilk
+#define spawn
+#define sync
+#define Self (0)
+#define SYNCHED (1)
+#define Cilk_active_size (1)
+#define Cilk_alloca (alloca)
+
+#include "mongen.cilk"
