02dbaa206fb54495dac80517d9c3cb67198768c1
Florent Hivert <Florent.Hivert@lri.fr>
2013-05-27 22:49:57 +0200

    Fixed Cilk termination

--- C-CILK-MMX/moncilk.pyx
+++ C-CILK-MMX/moncilk.pyx
@@ -6,25 +6,27 @@ cdef extern from "cilk.h":
     ctypedef struct CilkContext:
         pass
     cdef CilkContext *Cilk_init(int *, char**)
+    cdef void Cilk_terminate(CilkContext *)
 
 cdef extern from "mongen.h":
     cdef cmonoid.result *results_proc_local
     cdef cmonoid.monoid **stacks_proc_local
     cdef int nproc
-    cdef void walk_children_cilk "EXPORT(walk_children)"(CilkContext *context, cmonoid.monoid *pm)
+    cdef void walk_children_cilk "EXPORT(walk_children)"(CilkContext *, cmonoid.monoid *)
 
 cdef extern from "alarm.h":
      void start_alarm()
 
-cdef list argv = ["bla", "-nproc", "8"]
 
-cpdef unsigned long int callcilk(mon.Monoid m):
-    cdef char **c_argv = <char**>malloc(sizeof(char*) * len(argv))
+cpdef list callcilk(mon.Monoid m):
+    cdef list argv = ["bla", "-nproc", "8"]
+    cdef char *c_argv[3]
     cdef int argc = len(argv), proc, res = 0, i
     cdef CilkContext *context
+    cdef list resl
     global stacks_proc_local, results_proc_local, nproc
 
-    nproc=8
+    nproc=int(argv[2])
 
     # Init the various stacks
     stacks_proc_local = <cmonoid.monoid **> malloc(nproc*sizeof(cmonoid.monoid*));
@@ -40,21 +42,21 @@ cpdef unsigned long int callcilk(mon.Monoid m):
     for idx in range(len(argv)):
          c_argv[idx] = argv[idx]
 
+    print "GOOOOOOO !!!!!!!!"
+
     start_alarm()
     context = Cilk_init(&argc, c_argv)
-    print "GOOOOOOO !!!!!!!!"
     walk_children_cilk(context, &m._m)
+    Cilk_terminate (context);
 
+    resl = []
     for i in range(cmonoid.MAX_GENUS):
         res = 0
         for proc in range(nproc):
             res += results_proc_local[proc][i];
-        print "nmon(%s) = %s"%(i, res)
-    res = 0
-    for proc in range(nproc):
-        res += results_proc_local[proc][cmonoid.MAX_GENUS-1];
+        resl.append(res)
     for proc in range(nproc):
         free(stacks_proc_local[proc])
     free(stacks_proc_local)
     free(results_proc_local)
-    return res
+    return resl
