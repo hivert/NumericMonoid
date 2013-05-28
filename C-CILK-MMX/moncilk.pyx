80759def573c35f61d2833c7b8d00142a9c6a307
Florent Hivert <Florent.Hivert@lri.fr>
2013-05-28 12:25:54 +0200

    Cleanup in Cython-Cilk initialisation

--- C-CILK-MMX/moncilk.pyx
+++ C-CILK-MMX/moncilk.pyx
@@ -17,37 +17,55 @@ cdef extern from "mongen.h":
 cdef extern from "alarm.h":
      void start_alarm()
 
+cdef CilkContext *context = NULL
 
-cpdef list callcilk(mon.Monoid m):
+
+cpdef init():
     cdef list argv = ["bla", "-nproc", "8"]
     cdef char *c_argv[3]
-    cdef int argc = len(argv), proc, res = 0, i
-    cdef CilkContext *context
-    cdef list resl
-    global stacks_proc_local, results_proc_local, nproc
+    cdef int argc = len(argv), proc, i
+    global stacks_proc_local, results_proc_local, nproc, context
 
     nproc=int(argv[2])
 
-    # Init the various stacks
     stacks_proc_local = <cmonoid.monoid **> malloc(nproc*sizeof(cmonoid.monoid*));
     for proc in range(nproc):
         stacks_proc_local[proc] = <cmonoid.monoid *> malloc(100*sizeof(cmonoid.monoid));
-
-    # Init the proc local results
     results_proc_local = <cmonoid.result *> malloc(nproc*sizeof(cmonoid.result))
-    for proc in range(nproc):
-        for i in range(cmonoid.MAX_GENUS):
-            results_proc_local[proc][i] = 0;
+
 
     for idx in range(len(argv)):
          c_argv[idx] = argv[idx]
 
+    context = Cilk_init(&argc, c_argv)
+
+cpdef cleanup():
+    global stacks_proc_local, results_proc_local, context
+    Cilk_terminate (context)
+    context = NULL
+    for proc in range(nproc):
+        free(stacks_proc_local[proc])
+    free(stacks_proc_local)
+    free(results_proc_local)
+
+
+cpdef list callcilk(mon.Monoid m):
+    cdef int proc, res = 0, i
+    cdef list resl
+    global stacks_proc_local, results_proc_local, nproc
+
+
+    if context == NULL:
+        init()
+
+    for proc in range(nproc):
+        for i in range(cmonoid.MAX_GENUS):
+            results_proc_local[proc][i] = 0;
+
     print "GOOOOOOO !!!!!!!!"
 
     start_alarm()
-    context = Cilk_init(&argc, c_argv)
     walk_children_cilk(context, &m._m)
-    Cilk_terminate (context);
 
     resl = []
     for i in range(cmonoid.MAX_GENUS):
@@ -55,8 +73,6 @@ cpdef list callcilk(mon.Monoid m):
         for proc in range(nproc):
             res += results_proc_local[proc][i];
         resl.append(res)
-    for proc in range(nproc):
-        free(stacks_proc_local[proc])
-    free(stacks_proc_local)
-    free(results_proc_local)
     return resl
+
+init()
