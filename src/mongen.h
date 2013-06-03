5d09728cefb0163c1ea01ab20134d5a59bce816e
Florent Hivert <Florent.Hivert@lri.fr>
2013-06-03 11:37:21 +0200

    Add a target genus global variable to use with cython.

--- src/mongen.h
+++ src/mongen.h
@@ -3,6 +3,7 @@
 
 extern result *results_proc_local;
 extern monoid **stacks_proc_local;
+extern unsigned long int target_genus;
 extern int nproc;
 
 extern void EXPORT(walk_children)(CilkContext *context, monoid *pm);
