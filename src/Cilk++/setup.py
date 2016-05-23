2225fb3550c512d888707be2b96dea459a98c06d
Florent Hivert <Florent.Hivert@lri.fr>
2016-05-23 14:26:49 +0200

    Switching to git

--- src/Cilk++/setup.py
+++ src/Cilk++/setup.py
@@ -8,7 +8,8 @@ try:
 except KeyError:
     raise EnvironmentError, "Please define the CILK_ROOT environment variable !"
 # setup the gcc/cilk compiler
-os.environ['CC'] = os.path.join(CILK_ROOT, 'bin', 'g++')
+if 'CC' not in os.environ:
+    os.environ['CC'] = os.path.join(CILK_ROOT, 'bin', 'g++-5')
 
 
 from distutils.core import setup
@@ -17,21 +18,20 @@ from Cython.Distutils import build_ext
 from distutils.extension import Extension
 from sage.env import *
 
-SAGE_INC = os.path.join(SAGE_LOCAL, 'include')
-SAGE_C   = os.path.join(SAGE_SRC, 'c_lib', 'include')
-SAGE_DEV = os.path.join(SAGE_ROOT, 'src')
+#SAGE_INC = os.path.join(SAGE_LOCAL, 'include')
+#SAGE_C   = os.path.join(SAGE_SRC, 'c_lib', 'include')
+#SAGE_DEV = os.path.join(SAGE_ROOT, 'src')
 
 
 if platform.system()=="Darwin":
     MARCH='-march=corei7'
-    MTUNE='-march=corei7'   
-    CILK_LIB = os.path.join(CILK_ROOT, 'lib') 
-    
+    MTUNE='-march=corei7'
+    CILK_LIB = os.path.join(CILK_ROOT, 'lib')
 else:
     MARCH='-march=native'
     MTUNE='-march=native'
     CILK_LIB = os.path.join(CILK_ROOT, 'lib64')
- 
+    
 import Cython.Compiler.Options
 Cython.Compiler.Options.annotate = True
 
@@ -46,10 +46,10 @@ setup(
                                         MARCH, MTUNE,
                                         '-fcilkplus'],
                   define_macros = [('NDEBUG', '1'), ('MAX_GENUS','86')],
-                  include_dirs = [SAGE_C,SAGE_DEV],
+                  include_dirs = sage_include_directories(), # [SAGE_C,SAGE_DEV],
                   library_dirs = [CILK_LIB],
                   runtime_library_dirs = [CILK_LIB],
-                  libraries = ['csage', 'cilkrts'],
+                  libraries = ['cilkrts'],
                   ),
         ])
 
