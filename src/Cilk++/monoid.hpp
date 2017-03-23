8c895955cce867d14f28d48d89702caec801ceb2
Florent Hivert <Florent.Hivert@lri.fr>
2017-03-23 11:08:04 +0100

    monoid::iterator::count no longer consume children

--- src/Cilk++/monoid.hpp
+++ src/Cilk++/monoid.hpp
@@ -155,8 +155,8 @@ template<> inline generator_iter<CHILDREN>::generator_iter(const monoid &mon)
 template <class T> inline uint8_t generator_iter<T>::count()
 {
   uint8_t nbr = _mm_popcnt_u32(mask); // popcnt returns a 8 bits value
-  for (iblock++; iblock < bound; iblock++)
-    nbr += _mm_popcnt_u32(movemask_epi8(m.blocks[iblock] == block1));
+  for (ind_t ib = iblock+1; ib < bound; ib++)
+    nbr += _mm_popcnt_u32(movemask_epi8(m.blocks[ib] == block1));
   return nbr;
 };
 
